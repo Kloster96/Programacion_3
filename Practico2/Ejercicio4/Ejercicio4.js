28d314af0780485fcf189669353255cbb1fde112
Escalante Pablo <141648314+adamblade12@users.noreply.github.com>
2025-04-30 18:05:29 -0300

    Correccion Ejercicio 4

--- Practico2/Ejercicio4/Ejercicio4.js
+++ Practico2/Ejercicio4/Ejercicio4.js
@@ -1,16 +1,22 @@
 document.addEventListener('DOMContentLoaded', function(){
-    const formulario = document.getElementById("formulario");
     const lista = document.getElementById("lista");
     const nueva_tarea = document.getElementById("tarea_nueva");
-    formulario.addEventListener("submit", function(event){
-    event.preventDefault();
-    const tarea = nueva_tarea.value.trim();
-    if (tarea !== "") {
-        const nuevoItem = document.createElement("li");
-        nuevoItem.textContent = tarea;
-        lista.appendChild(nuevoItem);
-        nueva_tarea.value = ""; 
-    }
+    const boton = document.getElementById("boton");
+    boton.addEventListener("click", function(event){
+        event.preventDefault();
+        const tarea = nueva_tarea.value.trim();
+        if (tarea !== "") {
+            const nuevoItem = document.createElement("li");
+            nuevoItem.textContent = tarea;
+            nuevoItem.classList.add("tarea");
+            lista.appendChild(nuevoItem);
+            nueva_tarea.value = ""; 
+        }   
     });   
-
+    lista.addEventListener("click", function(evento){
+        if(evento.target.classList.contains("tarea")){
+            evento.target.classList.toggle("completado");
+        }
+            
+    });
 });
